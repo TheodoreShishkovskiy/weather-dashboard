fb5bda5f7aa35a2be82dc3881348dfc05d92b251
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-05 19:25:31 -0400

    starting main forecast function, currently not finished/working

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,28 @@ var handleErrors = (response) => {
   }
   return response;
 }
+
+var showCurrentWeather = (event) =>{
+  let city = $('weather-search').val();
+  currentCity = $('weather-search').val();
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+  fetch(queryURL)
+  .then(handleErrors)
+  .then((response) => {
+    saveCity(city);
+    $('weather-error').text("");
+    let weatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+    renderCities();
+    getWeatherForecast(event);
+    $('#header-title').text(response.name);
+    let weatherCurrentHTML =
+    <h3>${response.name} <img src="${currentWeatherIcon}"></h3>
+    <ul class="list-unstyled">
+      <li>Temperature: ${response.main.temp}&#8457;</li>
+      <li>Humidity: ${response.main.humidity}%</li>
+      <li>Wind Speed: ${response.wind.speed} MPH</li>
+    </ul>`;
+    $('weather-current').html(weatherCurrentHTML);
+    
+  })
+}
